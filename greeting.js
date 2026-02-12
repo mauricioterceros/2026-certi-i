bbd0be9f8c9c061a54082cbde13af764e5a65213
Mauricio Terceros <mauricio.terceros@gmail.com>
2026-02-12 19:54:57 -0400

    Updated the locale conditional

--- greeting.js
+++ greeting.js
@@ -2,7 +2,7 @@ var greetings;
 if (locale == 'es') {
 	greetings = esDeDia ? Buenos Dias : Buenas Noches
 
-} else if (locale == 'en') {
+} else if (locale == 'en' && locale == 'us') {
 	greetings = isMorning ? Good Morning : Good Afternoon
 
 }
